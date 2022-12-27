1ac67460fe2ddfb4e8fed1d78cf9c2f8c75dc80a
pandhupt <72847139+pagaspandhutanoyo@users.noreply.github.com>
2022-12-27 08:32:43 +0700

    Update DiskonModel.php

--- app/Models/DiskonModel.php
+++ app/Models/DiskonModel.php
@@ -9,11 +9,7 @@ class DiskonModel extends Model
     protected $table = 'diskon';
     protected $primaryKey = 'id';
     protected $allowedFields = [
-        'kode_voucher',
-        'tanggal_mulai_berlaku',
-        'tanggal_akhir_berlaku',
-        'besar_diskon',
-        'aktif'
+        'kode_voucher','tanggal_mulai_berlaku','tanggal_akhir_berlaku','besar_diskon','aktif'
     ];
     protected $returnType = 'App\Entities\Diskon';
     protected $useTimestamps = false;
